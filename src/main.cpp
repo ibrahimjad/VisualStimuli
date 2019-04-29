41ca3e38b99a1a45cecdd49fa4ec69485767b2ba
ibrahimjad <ibrahim.jad@outlook.dk>
2019-04-29 20:05:07 +0200

    Added 32bit support

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,19 @@
 
 int main() {
   VisualStimuli::Flicker flicker[] = {
-    VisualStimuli::Flicker(60.0f, 0.0f, 0.0f),
-    VisualStimuli::Flicker(30.0f, -0.75f, 0.75f),
-    VisualStimuli::Flicker(4.0f, 0.75f, 0.75f),
-    VisualStimuli::Flicker(2.0f, -0.75f, -0.75f),
-    VisualStimuli::Flicker(1.0f, 0.75f, -0.75f)
+    VisualStimuli::Flicker(2.0f, -0.75f, 0.75f),
+    VisualStimuli::Flicker(6.0f, 0.75f, 0.75f),
+    VisualStimuli::Flicker(12.0f, -0.75f, -0.75f),
+    VisualStimuli::Flicker(24.0f, 0.75f, -0.75f)
   };
 
-  VisualStimuli::Interface ui(flicker, 5);
+  VisualStimuli::Interface ui(flicker, 4);
   ui.initWindow();
 
-  float posX = -1.0f;
 
-  while (!ui.isQuit()) {
-    posX += 0.01;
-    posX = (posX <= 1.0f) ? posX : -1.0f;
-    flicker[0].setPosX(posX);
-    // flicker[0].setPosY(0.0f);
-    flicker[0].setColor(64, 64, 128);
-    flicker[0].setSize(20);
+  while (!ui.isQuit())
     ui.render();
-  }
+
 
   ui.quit();
 }
